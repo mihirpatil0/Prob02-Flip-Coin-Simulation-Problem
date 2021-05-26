8ca7668f62e61bf58bdd00956138cae94aa733e5
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-26 21:12:04 +0530

    Counted Who won how many times

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 echo "Welcome to Flip Coin Simulation"
 
-Random=$((RANDOM%2))
-if [ $Random -eq 1 ]
-then
-	echo "HEAD is Winner"
-else
-	echo "TAIL is winner"
-fi
+HEAD=0
+TAIL=0
+
+
+read -p "Enter the value of flip coins: " num
+for (( i=1; i<=$num; i++ ))
+do
+	Random=$((RANDOM%2))
+	if [ $Random -eq 1 ]
+	then
+#		echo "HEAD is Winner"
+		((HEAD++))
+	else
+#		echo "TAIL is winner"
+		((TAIL++))
+	fi
+done
+echo "HEAD won $HEAD times"
+echo "TAIL won $TAIL times"
+
+
