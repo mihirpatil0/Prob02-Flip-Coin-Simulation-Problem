3156f64492114d7ed8f1658d29dcaca8d1a13aee
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-26 23:19:06 +0530

    Added the code to continue the game till diffrenece is greater than 2

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -28,6 +28,33 @@ then
 	echo "TAIL won by $diff times more than HEAD"
 else
 	echo "IT'S TIE since their DIFFERENCE IS LESS-THAN-EQUAL-TO 2"
+	echo "By again flipping to make difference >2"
+        while [ 1 ]
+        do
+                RandomTwo=$((RANDOM%2))
+                if [ $RandomTwo -eq 1 ]
+                then
+                        ((++HEAD))
+                else
+                        ((++TAIL))
+                fi
+                dif1=$(($HEAD-$TAIL))
+                dif2=$(($TAIL-$HEAD))
+                if [[ $dif1 -gt 2 || $dif2 -gt 2 ]]
+                then
+                        break;
+                fi
+        done
+        echo "HEAD comes $HEAD times"
+        echo "TAIL comes $TAIL times"
+        if [ $HEAD -gt $(($TAIL)) ]
+        then
+        dif=$(($HEAD-$TAIL))
+        echo "HEAD won by $dif times more than TAIL"
+        else
+        dif=$(($TAIL-$HEAD))
+        echo "TAIL won by $dif times more than HEAD"
+        fi
 fi
 
 
